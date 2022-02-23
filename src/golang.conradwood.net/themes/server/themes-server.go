a276cc483d0c29de68692e0606492bbfac7a56ce
Conrad Wood <cnw@conradwood.net>
2022-02-23 19:24:14 +0000

    cache file content
    Change-Id: I4e3c30a47e0c98483c79dbd4e30d3b0d2841bc0c

--- src/golang.conradwood.net/themes/server/themes-server.go
+++ src/golang.conradwood.net/themes/server/themes-server.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"golang.conradwood.net/apis/h2gproxy"
 	pb "golang.conradwood.net/apis/themes"
+	"golang.conradwood.net/go-easyops/cache"
 	"golang.conradwood.net/go-easyops/server"
 	"golang.conradwood.net/go-easyops/utils"
 	"google.golang.org/grpc"
 	"os"
 	"strings"
+	"time"
 )
 
 const (
@@ -19,9 +21,15 @@ const (
 )
 
 var (
-	port = flag.Int("port", 4100, "The grpc server port")
+	port       = flag.Int("port", 4100, "The grpc server port")
+	file_cache = cache.New("filecontent", time.Duration(150)*time.Hour, 100)
+	fdir       = ""
 )
 
+type fcache struct {
+	Data  []byte
+	Error error
+}
 type echoServer struct {
 }
 
@@ -64,16 +72,14 @@ func (e *echoServer) GetThemeByHost(ctx context.Context, req *pb.HostThemeReques
 			HeaderText:    "SingingCat - IoT as a Service",
 			ThemeName:     "singingcat",
 		}
-	}
-	if strings.Contains(req.Host, "youritguru") {
+	} else if strings.Contains(req.Host, "youritguru") {
 		res = &pb.ThemeResponse{
 			SmallLogoName: "guru.jpeg",
 			FavIconName:   "favicon.ico",
 			HeaderText:    "The IT Guru - All of Information Technology as a Service",
 			ThemeName:     "youritguru",
 		}
-	}
-	if strings.Contains(req.Host, "ionos") {
+	} else if strings.Contains(req.Host, "ionos") {
 		res = &pb.ThemeResponse{
 			SmallLogoName: "ionos_logo.png",
 			FavIconName:   "ionos.ico",
@@ -89,17 +95,24 @@ func (e *echoServer) GetHTMLTheme(ctx context.Context, req *h2gproxy.ServeReques
 	return e.GetThemeByHost(ctx, &pb.HostThemeRequest{Host: req.Host})
 }
 func (e *echoServer) getFileForTheme(ctx context.Context, req *pb.HostThemeRequest, filename string) ([]byte, error) {
-	f, err := utils.FindFile("templates/")
-	if err != nil {
-		return nil, err
+	if fdir == "" {
+		f, err := utils.FindFile("templates/")
+		if err != nil {
+			return nil, err
+		}
+		fdir = f
 	}
 	t, err := e.GetThemeByHost(ctx, req)
 	if err != nil {
 		return nil, err
 	}
-	d_filename := f + "/" + t.ThemeName + "/" + filename
+	d_filename := fdir + "/" + t.ThemeName + "/" + filename
+	fc := file_cache.Get(d_filename)
+	if fc != nil {
+		return fc.(*fcache).Data, fc.(*fcache).Error
+	}
 	if !utils.FileExists(d_filename) {
-		d_filename = f + "/default/" + filename
+		d_filename = fdir + "/default/" + filename
 	}
 	if !utils.FileExists(d_filename) {
 		fmt.Printf("Warning file \"%s\" does not exist", d_filename)
@@ -109,6 +122,7 @@ func (e *echoServer) getFileForTheme(ctx context.Context, req *pb.HostThemeReque
 	if err != nil {
 		return nil, err
 	}
+	file_cache.Put(d_filename, &fcache{Data: u})
 	return u, nil
 }
 func (e *echoServer) GetLogo(ctx context.Context, req *pb.HostThemeRequest) (*pb.Image, error) {
